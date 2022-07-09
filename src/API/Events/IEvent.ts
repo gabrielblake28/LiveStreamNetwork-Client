b4bd8df07acd89f16cca74e3ce5ed7dab5dac1d7
Gabriel Blake <gabriel.blake28@gmail.com>
2022-07-08 21:04:44 -0700

    revert

--- src/API/Events/IEvent.ts
+++ src/API/Events/IEvent.ts
@@ -4,11 +4,11 @@ export interface IEvent {
   start_timestamp: Date;
   end_timestamp: Date;
   image: string;
-  featured?: boolean;
+  featured: boolean;
   user_id: string;
   category_id: string;
-  event_id?: string;
+  event_id: string;
   profile_pic?: string;
   name?: string;
-  subscription_id?: string;
+  subscription_id: string;
 }
